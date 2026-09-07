3e53913e33812bdeec6f0d8a0a0fa4243c2e4ad5
Emmanuel <animj.emma@gmail.com>
2026-09-07 20:17:01 +0100

    trying out if path

--- src/animtry2/Hacker1Printouts.java
+++ src/animtry2/Hacker1Printouts.java
@@ -5,9 +5,20 @@ import java.util.Scanner;
 public class Hacker1Printouts {
 	public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+//        
+//        if(scan.hasNextInt()) {
+//       	 i = scan.nextInt();
+//       }else if(scan.hasNext()) {
+//       	 s = scan.next();
+//       }else if(scan.hasNextDouble()) {
+//       	d = scan.nextDouble();
+//       };
+       
+       
         int i = scan.nextInt();
         
-
+        String s = scan.next();
+        double d = scan.nextDouble();
         // Write your code here.
 
         System.out.println("String: " + s);
